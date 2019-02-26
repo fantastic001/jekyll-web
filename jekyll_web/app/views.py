1d445d30747ad6a14088cc541baf467eddf47a1e
Stefan Nožinić <stefan@lugons.org>
2019-02-27 00:57:21 +0100

    fixes

--- jekyll_web/app/views.py
+++ jekyll_web/app/views.py
@@ -10,7 +10,7 @@ from pyjekyll import *
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from django.urls import reverse
 from jekyll_web import settings
 
 def index(request):
--- jekyll_web/jekyll_web/urls.py
+++ jekyll_web/jekyll_web/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r"^$", views.index, name="index"),
-    path(r"^accounts/login/", views.user_login, name="login"),
-    path(r"^accounts/logout/", views.user_logout, name="logout"),
-    path(r"^articles/$", views.article_list, name="article-list")
+    path(r'admin/', admin.site.urls),
+    path(r"", views.index, name="index"),
+    path(r"accounts/login/", views.user_login, name="login"),
+    path(r"accounts/logout/", views.user_logout, name="logout"),
+    path(r"articles/", views.article_list, name="article-list")
     # path(r'^(?P<username>\w+)/blog/', include('foo.paths.blog')),
     # path(r'^articles/([0-9]{4})/$', views.year_archive, name='news-year-archive'),
 ]
